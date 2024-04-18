a68ef8e6974e32b7abb9fae1d411a08d3b21de29
rhyg <rhysgeary@gmail.com>
2024-04-19 06:44:08 +1000

    Added @datacms/svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+	import { Image } from '@datocms/svelte';
+</script>
+
+<h1>DatoCMS Svelte Image component repro</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
